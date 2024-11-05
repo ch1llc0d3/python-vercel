7524e0a920287bb0bcc93c0ac6c48af0ff9f10dd
ch1llc0d3 <chillcrib7@gmail.com>
2024-11-05 07:55:28 -0300

    allowed hosts

--- python_vercel_task/settings.py
+++ python_vercel_task/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['*']  # or ['your-vercel-domain.vercel.app']
 
 
 # Application definition
