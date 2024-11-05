66260ae4e5b2974dde6720ffdd633e73b9bd6dd2
ch1llc0d3 <chillcrib7@gmail.com>
2024-11-05 08:02:48 -0300

    lambda

--- python_vercel_task/urls.py
+++ python_vercel_task/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path("", lambda request: HttpResponseRedirect("blog/")),  # Redirect root URL to /blog/
 ]
